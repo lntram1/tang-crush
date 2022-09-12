8ad9526b62f8a80edb8d9daa6ddd55a78e75569f
lntram1 <113273903+lntram1@users.noreply.github.com>
2022-09-12 20:59:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Ái phi à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao nàng cứ đi nhầm đường
-    Lạc vào tim trẫm lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng nàng đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng nàng đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích trẫm <33',
-    btnNo: 'Không:3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao nàng lại yêu ta <3',
-    btnReply: 'Gửi cho <3',
+    introTitle: 'Hế lu a Bảo=)))',
+    introDesc: `Chơi đố zui nè, đố trả lời đc, ko trả lời đc lm tó=))))`,
+    btnIntro: 'OK',
+    title: 'What is ligma?',
+    desc: 'Đố trả lời được(・∀・)',
+    btnYes: 'Ligma what???',
+    btnNo: 'Không biết:3',
+    question:'Gửi đáp án đê',
+    btnReply: 'Gửi<3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'T biết mà 🥰. Thích t rồi chứ j=)) 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Ligma balls=)))))',
+    messDesc: 'Đoán sai r chứ j( ͡° ͜ʖ ͡°), thế lm tó nhá',
+    btnAccept: 'Okiiiii lun',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
